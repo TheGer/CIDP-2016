1522ef3726a3f90fa5d2fd67a3aa998b1d91325c
Gerry Said <gerrysaid@gmail.com>
2016-02-16 13:28:35 +0100

    Lesson 2
    Added show hide images and next/previous buttons

--- Assets/Scripts/buttonManager.cs
+++ Assets/Scripts/buttonManager.cs
@@ -24,6 +24,40 @@ public class buttonManager : MonoBehaviour {
 		GameObject.Find ("OutputText").GetComponent<Text> ().text = input;
 	}
 
+	//code which is triggered when the smiley button is pressed
+	public void button2Pressed()
+	{
+		//1. check if the image is visible
+		//2. if the image is visible, hide the image
+		//3. if the image is hidden, show the image
+		bool imageVisible = true;
+
+		//return true if the image is visible or false if the image is not
+		imageVisible = GameObject.Find ("Smiley").GetComponent<RawImage> ().enabled;
+
+		if (imageVisible == true) {
+			//hide the image
+			GameObject.Find ("Smiley").GetComponent<RawImage> ().enabled = false;
+		} else {
+			//show the image
+			GameObject.Find ("Smiley").GetComponent<RawImage> ().enabled = true;
+		}
+
+
+	}
+
+	//code which is triggered when the next button is pressed
+	public void nextButtonPressed()
+	{
+		Application.LoadLevel ("scene2");
+	}
+
+	//code which is triggered when the previous button is pressed
+	public void previousButtonPressed()
+	{
+		Application.LoadLevel ("scene1");
+	}
+
 
 	// Use this for initialization
 	void Start () {
